81b68e2ffbccc9be985af845e9f6b4b0975be4e3
preet patel <preetpatelofficial004@gmail.com>
2023-08-23 15:01:21 -0400

    profile page fix

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -7,22 +7,35 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
         <span className="orange_gradient">{name} Profile</span>
       </h1>
       <p className="desc text-left">{desc}</p>
-      <div className='feed'>
-        <div className={`${data.length === 1 ? '' :'w-full sm:w-auto prompt_grid'}`}>
-          {data &&
-            data.map((post) => (
-              <PromptCard
-                key={post._id}
-                post={post}
-                handleDelete={() => {
-                  handleDelete && handleDelete(post);
-                }}
-                handleEdit={() => {
-                  handleEdit && handleEdit(post);
-                }}
-              />
-            ))}
-        </div>
+      <div className="feed">
+        {data.length !== 0 ? (
+          <div
+            className={`${
+              data.length === 1 ? "" : "w-full sm:w-auto prompt_grid"
+            }`}
+          >
+            {data &&
+              data.map((post) => (
+                <PromptCard
+                  key={post._id}
+                  post={post}
+                  handleDelete={() => {
+                    handleDelete && handleDelete(post);
+                  }}
+                  handleEdit={() => {
+                    handleEdit && handleEdit(post);
+                  }}
+                />
+              ))}
+          </div>
+        ) : (
+          <div className="mt-16 w-full flex justify-center items-center">
+            <p className="text-md md:text-xl font-semibold opacity-80 text-gray-400">
+              You Do not have any Posts yet!
+            </p>
+            
+          </div>
+        )}
       </div>
     </section>
   );
